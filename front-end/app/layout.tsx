f9f560dda62d5c3c955f97d83d2d1c3f4d5dcec3
pedrormelo <sakki.backup@gmail.com>
2025-04-09 11:51:48 -0300

    lady writer on the tv

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Paycheck',
+  description: 'help of v0 ai',
   generator: 'v0.dev',
 }
 
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="br">
       <body>{children}</body>
     </html>
   )
