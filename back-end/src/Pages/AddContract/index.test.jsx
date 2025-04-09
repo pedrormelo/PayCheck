867a5f6b0c8dd66b058d5d27d42ccd4b663e73ee
pedrormelo <sakki.backup@gmail.com>
2025-04-09 10:32:57 -0300

    time pink floyd

--- back-end/src/Pages/AddContract/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import AddContractPage from "./index";
-
-test("renders AddContractPage component", () => {
-    render(<AddContractPage />);
-    const titleElement = screen.getByText(/Cadastrar Contrato/i);
-    expect(titleElement).toBeInTheDocument();
-});
